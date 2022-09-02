de79aa53707a9189ecd33626c41a05014e32a6e3
Juan Ignacio Delena <juanignaciodelena@MacBook-Air-de-Juan-Ignacio.local>
2022-09-01 22:29:04 -0300

    Se subio el entregable de la materia FrontEnd III

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-// El componente App es el padre de:
-// - Cabecera
-// - Listado
-// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
-// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
-// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+import React from "react";
+import Cabecera from "./components/Cabecera";
+import Listado from "./components/Listado";
+import { useState } from "react";
 
 function App() {
+  const [cuenta, setCuenta] = useState(0);
+
+  const incrementador = (event) => {
+    setCuenta(cuenta + 1);
+  };
 
   return (
     <div className="App">
-      <Cabecera />
-      <Listado />
+      <Cabecera cantidad={cuenta} />
+      <Listado incrementador={incrementador} />
     </div>
   );
 }
